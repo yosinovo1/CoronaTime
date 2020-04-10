32be2e599218c793e628575d509e2130f88d6828
Gilad Leifman <leifmangilad@gmail.com>
2020-04-10 16:36:23 +0300

    Reformated and treating space as and in the search

--- Programming Languages/JS/React/projects_viewer/src/components/layout/Header.js
+++ Programming Languages/JS/React/projects_viewer/src/components/layout/Header.js
@@ -1,103 +1,133 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
 
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { getProjects, updateSelectedProject, updateFilteredProjects } from '../../actions/projects';
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import {
+  getProjects,
+  updateSelectedProject,
+  updateFilteredProjects,
+} from "../../actions/projects";
 
-
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 
 class Header extends Component {
-    static propTypes = {
-        projects: PropTypes.array.isRequired,
-        selectedProject: PropTypes.object
-    }
+  static propTypes = {
+    projects: PropTypes.array.isRequired,
+    selectedProject: PropTypes.object,
+  };
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.props.getProjects();
-        this.searchFields = ["serialNumber", "location", "address", "customer"];
-        this.classes = {
-            root: {
-                padding: '2px 4px',
-                display: 'flex',
-                alignItems: 'center',
-                width: 400,
-            },
-            input: {
-                flex: 1,
-            },
-            iconButton: {
-                padding: 10,
-            },
-            divider: {
-                height: 28,
-                margin: 4,
-            },
-        }
-    }
+    this.props.getProjects();
+    this.searchFields = ["serialNumber", "location", "address", "customer"];
+    this.classes = {
+      root: {
+        padding: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 400,
+      },
+      input: {
+        flex: 1,
+      },
+      iconButton: {
+        padding: 10,
+      },
+      divider: {
+        height: 28,
+        margin: 4,
+      },
+    };
+  }
 
-    filterProjects(inputValue) {
-        let filteredProjects = this.props.projects.filter(projectData => this.searchFieldsInProject(projectData, this.searchFields, inputValue));
-        this.props.updateFilteredProjects(filteredProjects);
-    }
-
-    searchFieldsInProject(projectData, searchFields, inputValue) {
-        let shouldAdd = false;
-
-        searchFields.forEach(field => {
-            if (!shouldAdd && projectData[field]) {
-                if (typeof projectData[field] === "object") {
-                    // Search in all keys
-                    shouldAdd = this.searchFieldsInProject(projectData[field], Object.keys(projectData[field]), inputValue)
-                }
-
-                else if (projectData[field].toString().toLowerCase().includes(inputValue.toLowerCase())) {
-                    shouldAdd = true;
-                }
-            }
-        });
-        return shouldAdd;
-    }
-
-
-    render() {
-        return (
-            <div style={{ "padding": "0.5rem", "backgroundColor": "rgba(255, 255, 255, 0.08)", "display": "flex", "justifyContent": "space-between", "flexDirection": "row-reverse" }}>
-                <Typography variant="h6">
-                    פרוייקטים
-                </Typography>
-                <Paper component="form" className={this.classes.root}>
-                    <InputBase
-                        placeholder="...חפש בפרוייקטים"
-                        onChange={(e) => this.filterProjects(e.target.value)}
-                        autoFocus
-                        inputProps={{
-                            style: { textAlign: "right", width: "33vw", "padding": "0.5rem" }
-                        }}
-                    />
-                    <IconButton className={this.classes.iconButton} aria-label="search">
-                        <SearchIcon />
-                    </IconButton>
-                </Paper>
-                <Typography variant="h6">
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                </Typography>
-            </div>
+  filterProjects(inputValues) {
+    let filteredProjects = this.props.projects.filter((projectData) =>
+      inputValues
+        .split(/ /)
+        .filter((inputValue) => !!inputValue)
+        .reduce(
+          (flag, inputValue) =>
+            this.searchFieldsInProject(
+              projectData,
+              this.searchFields,
+              inputValue
+            ),
+          true
         )
-    }
+    );
+    this.props.updateFilteredProjects(filteredProjects);
+  }
+
+  searchFieldsInProject(projectData, searchFields, inputValue) {
+    let shouldAdd = false;
+    searchFields.forEach((field) => {
+      if (!shouldAdd && projectData[field]) {
+        if (typeof projectData[field] === "object") {
+          // Search in all keys
+          shouldAdd = this.searchFieldsInProject(
+            projectData[field],
+            Object.keys(projectData[field]),
+            inputValue
+          );
+        } else if (
+          projectData[field]
+            .toString()
+            .toLowerCase()
+            .includes(inputValue.toLowerCase())
+        ) {
+          shouldAdd = true;
+        }
+      }
+    });
+    return shouldAdd;
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          padding: "0.5rem",
+          backgroundColor: "rgba(255, 255, 255, 0.08)",
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "row-reverse",
+        }}
+      >
+        <Typography variant="h6">פרוייקטים</Typography>
+        <Paper component="form" className={this.classes.root}>
+          <InputBase
+            placeholder="חפש בפרוייקטים..."
+            onChange={(e) => this.filterProjects(e.target.value)}
+            autoFocus
+            inputProps={{
+              style: { textAlign: "right", width: "33vw", padding: "0.5rem" },
+            }}
+          />
+          <IconButton className={this.classes.iconButton} aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+        <Typography variant="h6">
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        </Typography>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => ({
-    projects: state.projects.projects,
-    selectedProject: state.projects.selectedProject
+const mapStateToProps = (state) => ({
+  projects: state.projects.projects,
+  selectedProject: state.projects.selectedProject,
 });
 
-export default connect(mapStateToProps, { getProjects, updateSelectedProject, updateFilteredProjects })(Header);
+export default connect(mapStateToProps, {
+  getProjects,
+  updateSelectedProject,
+  updateFilteredProjects,
+})(Header);
