d5d1f469b345096c0cd999601984ae4795b7e953
dennisg11 <denisgerman11@gmail.com>
2020-04-22 16:33:43 +0300

    CR

--- Gilad And Dennis Projects/Backend/ShopSite/urls.py
+++ Gilad And Dennis Projects/Backend/ShopSite/urls.py
@@ -21,16 +21,16 @@ from shop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('songs/', views.song_list),
-    path('songs/<int:pk>', views.song_detail),
-    path('songs/by_substring/<str:substring>', views.song_substring_filter),
-    path('songs/by_album/<str:albumname>/<str:artistname>', views.song_album_filter),
-    path('songs/by_artist/<str:artistname>', views.song_artist_filter),
-    path('albums/by_substring/<str:substring>', views.album_substring_filter),
-    path('albums/by_artist/<str:artistname>', views.album_artist_filter),
-    path('albums/<str:albumname>/<str:artistname>', views.album_detail),
-    path('artists/<str:artistname>', views.artist_detail),
-    path('artists/by_substring/<str:substring>', views.artist_substring_filter),
+    path('songs/', views.SongList.as_view()),
+    path('songs/<int:pk>', views.SongDetail.as_view()),
+    path('songs/by_substring/<str:substring>', views.SongSubstringFilter.as_view()),
+    path('songs/by_album/<str:albumname>/<str:artistname>', views.SongAlbumFilter.as_view()),
+    path('songs/by_artist/<str:artistname>', views.SongArtistFilter.as_view()),
+    path('albums/by_substring/<str:substring>', views.AlbumSubstringFilter.as_view()),
+    path('albums/by_artist/<str:artistname>', views.AlbumArtistFilter.as_view()),
+    path('albums/<str:albumname>/<str:artistname>', views.AlbumDetail.as_view()),
+    path('artists/<str:artistname>', views.ArtistDetail.as_view()),
+    path('artists/by_substring/<str:substring>', views.ArtistSubstringFilter.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- Gilad And Dennis Projects/Backend/shop/views.py
+++ Gilad And Dennis Projects/Backend/shop/views.py
@@ -5,25 +5,26 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from shop.models import Song
 from shop.serializers import SongSerializer
+from rest_framework.views import APIView
+from rest_framework import generics
 
 
-@api_view(['GET', 'PUT', 'POST'])
-def song_list(request, format=None):
+# I did not use generics here since there wasn't an appropriate generic class for my requirements
+class SongList(APIView):
     """
-    List all songs, or create a new song/s.
+    List all songs, or create a new songs/s.
     """
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
-
-    if request.method == 'GET':
+    def get(self, request, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
         songs = Song.objects.all()
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         data = request.data
         if isinstance(data, list):
             serializer = SongSerializer(data=request.data, many=True)
@@ -35,8 +36,7 @@ def song_list(request, format=None):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-    elif request.method == 'PUT':
+    def put(self, request, format=None):
         for put_request in request.data:
             try:
                 song = Song.objects.get(id=put_request["id"])
@@ -49,125 +49,99 @@ def song_list(request, format=None):
         # using initial data since all data has been validated already
         return Response(SongSerializer(data=request.data, many=True).initial_data)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def song_detail(request, pk, format=None):
-    """
-    Retrieve, update or delete a song.
-    """
+# If i will need to add content filters (whether the details are global or not)
+# I'll override some functions with the is_global checking
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Song.objects.all()
+    serializer_class = SongSerializer
+    
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
-
-    try:
-        song = Song.objects.get(pk=pk)
-    except Song.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = SongSerializer(song)
-        return Response(serializer.data)
-
-    elif request.method == 'PUT':
-        serializer = SongSerializer(song, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    elif request.method == 'DELETE':
-        song.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-@api_view(['GET'])
-def song_substring_filter(request, substring, format=None):
+class SongSubstringFilter(generics.ListAPIView):
     """
     List all songs that contain a substring in their track name.
     """
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
-
-    if request.method == 'GET':
+    def get(self, request, substring, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
         songs = Song.objects.filter(track_name__icontains=substring)
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def song_album_filter(request, albumname, artistname, format=None):
+
+class SongAlbumFilter(generics.ListAPIView):
     """
     List all songs in a specific album. 
     """
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
-
-    if request.method == 'GET':
+    def get(self, request, albumname, artistname, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
         songs = Song.objects.filter(album_name=albumname).filter(artist_name=artistname)
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def song_artist_filter(request, artistname, format=None):
+
+class SongArtistFilter(generics.ListAPIView):
     """
     List all songs by a specific artist. 
     """
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
-
-    if request.method == 'GET':
+    def get(self, request, artistname, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
         songs = Song.objects.filter(artist_name=artistname)
         serializer = SongSerializer(songs, many=True)
         return Response(serializer.data)
 
-@api_view(['GET'])
-def album_detail(request, albumname, artistname, format=None):
+
+class AlbumDetail(generics.ListAPIView):
     """
     Retrieve an album.
     """
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
+    def get(self, request, albumname, artistname, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
+        songs = Song.objects.filter(artist_name=artistname)
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
 
-    songs = Song.objects.filter(album_name=albumname).filter(artist_name=artistname)
-    serializer = SongSerializer(songs, many=True)
-    try:
-        album = {"album_name": f"{serializer.data[0]['album_name']}", "release_date": "Currently not available",
-            "artist_name": f"{serializer.data[0]['artist_name']}", "genre": "Currently not available"}
-    except IndexError:
-        return Response(status.HTTP_404_NOT_FOUND) 
+        songs = Song.objects.filter(album_name=albumname).filter(artist_name=artistname)
+        serializer = SongSerializer(songs, many=True)
+        try:
+            album = {"album_name": f"{serializer.data[0]['album_name']}", "release_date": "Currently not available",
+                "artist_name": f"{serializer.data[0]['artist_name']}", "genre": "Currently not available"}
+        except IndexError:
+            return Response(status.HTTP_404_NOT_FOUND) 
             
-    if request.method == 'GET':
         return Response(album)
 
-@api_view(['GET'])
-def album_substring_filter(request, substring, format=None):
+
+class AlbumSubstringFilter(generics.ListAPIView):
     """
     List all albums that contain a substring in their track name.
     """
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
+    def get(self, request, substring, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
 
-    if request.method == 'GET':
         album_list = []
         songs = Song.objects.filter(album_name__icontains=substring)
         serializer = SongSerializer(songs, many=True)
@@ -179,19 +153,18 @@ def album_substring_filter(request, substring, format=None):
                 album_list.append(album)
         return Response(album_list)
 
-@api_view(['GET'])
-def album_artist_filter(request, artistname, format=None):
+
+class AlbumArtistFilter(generics.ListAPIView):
     """
     List all albums by a specific artist. 
     """
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
-
-    if request.method == 'GET':
+    def get(self, request, artistname, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
         songs = Song.objects.filter(artist_name=artistname)
         serializer = SongSerializer(songs, many=True)
         song = serializer.data[0]
@@ -199,40 +172,39 @@ def album_artist_filter(request, artistname, format=None):
                 "artist_name": f"{song['artist_name']}", "genre": "Currently not available"}
         return Response(album)
 
-@api_view(['GET'])
-def artist_detail(request, artistname, format=None):
+
+class ArtistDetail(generics.ListAPIView):
     """
     Retrieve an artist.
     """
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
-
-    songs = Song.objects.filter(artist_name=artistname)
-    serializer = SongSerializer(songs, many=True)
-    try:
-        artist = {"artist_name": f"{serializer.data[0]['artist_name']}"}   
-    except IndexError:
-        return Response(status.HTTP_404_NOT_FOUND)   
-    if request.method == 'GET':
+    def get(self, request, artistname, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
+        songs = Song.objects.filter(artist_name=artistname)
+        serializer = SongSerializer(songs, many=True)
+        try:
+            artist = {"artist_name": f"{serializer.data[0]['artist_name']}"}   
+        except IndexError:
+            return Response(status.HTTP_404_NOT_FOUND)   
         return Response(artist)
 
-@api_view(['GET'])
-def artist_substring_filter(request, substring, format=None):
+
+class ArtistSubstringFilter(generics.ListAPIView):
     """
     List all artists that contain a substring in their name.
     """
 
-    try:
-        is_global = request.content_params["is_global"]
-    except KeyError:
-        # By default show global data
-        is_global = True
-    
-    if request.method == 'GET':
+    def get(self, request, substring, format=None):
+        try:
+            is_global = request.content_params["is_global"]
+        except KeyError:
+            # By default show global data
+            is_global = True
+            
         artist_list = []
         songs = Song.objects.filter(artist_name__icontains=substring)
         serializer = SongSerializer(songs, many=True)
